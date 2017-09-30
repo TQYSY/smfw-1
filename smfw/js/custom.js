c7beb23122ea219abd4bea083f24bf33561cb8c7
Ubuntu <arthur@bee.bee.i7.internal.cloudapp.net>
2017-09-30 21:00:26 +0800

    v1

---
+++ smfw/js/custom.js
@@ -0,0 +1,207 @@
+                                
+
+jQuery(function ($) { "use strict";
+	/* =========================================================================== */
+	/*	FitVids js
+	/* =========================================================================== */
+	
+	$(".media-wrapper").fitVids();
+
+	
+	/* ========================================================================= */
+	/*	Nice Scroll - Custom Scrollbar
+	/* ========================================================================= */
+
+	var nice = $("html").niceScroll({
+		cursorborderradius: 0,
+		cursorwidth: "8px",
+		cursorfixedheight: 150,
+		cursorcolor: "#6CB670",
+		zindex: 9999,
+		cursorborder: 0,
+	});
+
+
+	/* ========================================================================= */
+	/*	Scroll Up / Back to top
+	/* ========================================================================= */
+	
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 400) {
+			$("#scrollUp").fadeIn(200);
+		} else {
+			$("#scrollUp").fadeOut(200);
+		}
+	});
+	
+	$('#scrollUp').click(function() {
+		$('html, body').stop().animate({
+			scrollTop : 0
+		}, 1500, 'easeInOutExpo');
+	});
+
+
+	/* ========================================================================= */
+	/*	Post image slider
+	/* ========================================================================= */
+	
+	$("#post-thumb, #gallery-post").owlCarousel({
+
+		navigation : true,
+		pagination : false,
+		slideSpeed : 800,
+		autoHeight : true,
+		paginationSpeed : 800,
+		singleItem:true,
+		navigationText : ["<i class='fa fa-angle-left fa-2x'></i>","<i class='fa fa-angle-right fa-2x'></i>"]
+
+	});
+	
+	$("#features").owlCarousel({
+		navigation : false,
+		pagination : true,
+		slideSpeed : 800,
+		singleItem : true,
+		transitionStyle : "fadeUp",
+		paginationSpeed : 800,
+	});
+
+
+	/* ========================================================================= */
+	/*	Menu item highlighting
+	/* ========================================================================= */
+
+
+	$("#navigation").sticky({
+		topSpacing : 0
+	});
+	
+	$('#nav').onePageNav({
+		currentClass: 'current',
+		changeHash: false,
+		scrollSpeed: 1500,
+		scrollThreshold: 0.5,
+		filter: '',
+		easing: 'easeInOutExpo'
+	});
+	
+
+
+	/* ========================================================================= */
+	/*	Fix Slider Height
+	/* ========================================================================= */	
+
+	var slideHeight = $(window).height();
+	
+	$('#slitSlider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('#slitSlider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
+	});
+	
+	
+
+	/* ========================================================================= */
+	/*	Timer count
+	/* ========================================================================= */
+
+	(function() {
+		var count = {
+		  initialized : false,
+		  initialize : function() {
+			if (this.initialized)
+			  return;
+			this.initialized = true;
+			this.build();
+		  },
+		  build : function() {
+			this.animations();
+		  },
+		  animations : function() {
+			// Count To
+			$(".counters-item [data-to]").each(function() {
+			  var $this = $(this);
+			  $this.appear(function() {
+				$this.countTo({});
+			  }, {
+				accX : 0,
+				accY : -150
+			  });
+			});
+		  },
+		};
+		count.initialize();
+	})();
+
+
+	/* ========================================================================= */
+	/*	Skills Chart
+	/* ========================================================================= */
+
+	$(".chart").appear(function () {
+		$(".chart").easyPieChart({
+			easing: "easeOutBounce",
+			barColor: "#6CB670",
+			size: "150",
+			lineWidth: 15,
+			animate: 2e3,
+			onStep: function (e, t, n) {
+				$(this.el).find(".percent").text(Math.round(n))
+			}
+		})
+	});
+
+
+	/* ========================================================================= */
+	/*	Portfolio Filtering Hook
+	/* =========================================================================  */
+
+	$('#og-grid').mixItUp(); // Portfolio filter
+
+	Grid.init(); //Portfolio Grid Expand
+	
+	
+	/* ========================================================================= */
+	/*	Testimonial Carousel
+	/* =========================================================================  */
+ 
+	//Init the carousel
+	$("#testimonials").owlCarousel({
+		slideSpeed: 500,
+		paginationSpeed: 500,
+		singleItem: true,
+		pagination : true,
+		transitionStyle : "backSlide"
+	});
+
+});
+
+/* ========================================================================= */
+/*	On scroll fade/bounce fffect
+/* ========================================================================= */
+
+	wow = new WOW({
+		animateClass: 'animated',
+		offset: 120
+	});
+	wow.init();
+
+
+
+/* ========================================================================= */
+/*	Parallax Sections
+/* ========================================================================= */
+
+
+"use strict";
+
+function parallaxInit() {
+	$('#counter').parallax("50%", 0.3);
+	$('#team-skills').parallax("50%", 0.3);
+	$('#twitter-feed').parallax("50%", 0.3);
+	$('#testimonial').parallax("50%", 0.3);
+}
+
+$(window).bind("load", function () {
+    parallaxInit()
+});
